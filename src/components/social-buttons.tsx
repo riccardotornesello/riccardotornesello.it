e8a72bc37ef1e436a7c146dfa1953a59481545e2
riccardotornesello <riccardo.tornesello@gmail.com>
2023-05-20 16:50:59 +0400

    improve social buttons

--- src/components/social-buttons.tsx
+++ src/components/social-buttons.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { ActionIcon, createStyles } from '@mantine/core';
+import { ActionIcon, ThemeIcon, createStyles } from '@mantine/core';
 import { IconBrandLinkedin, IconBrandGithub } from '@tabler/icons-react';
 
 const useStyles = createStyles((theme) => ({
@@ -12,20 +12,41 @@ const useStyles = createStyles((theme) => ({
   },
 }));
 
+export type SocialButtonProps = {
+  className: string;
+  href: string;
+  children: React.ReactNode;
+};
+
+export function SocialButton({
+  className,
+  href,
+  children,
+}: SocialButtonProps): JSX.Element {
+  return (
+    <a target='_blank' href={href}>
+      <ThemeIcon
+        size='3.5rem'
+        radius='3.5rem'
+        variant='filled'
+        className={className}
+      >
+        {children}
+      </ThemeIcon>
+    </a>
+  );
+}
+
 export function LinkedInButton(): JSX.Element {
   const { classes } = useStyles();
 
   return (
-    <ActionIcon
-      size='3.5rem'
-      radius='3.5rem'
-      variant='filled'
-      component='a'
-      href='/asd'
+    <SocialButton
       className={classes.linkedin}
+      href='https://www.linkedin.com/in/riccardotornesello/'
     >
       <IconBrandLinkedin size='2rem' />
-    </ActionIcon>
+    </SocialButton>
   );
 }
 
@@ -33,15 +54,11 @@ export function GitHubButton(): JSX.Element {
   const { classes } = useStyles();
 
   return (
-    <ActionIcon
-      size='3.5rem'
-      radius='3.5rem'
-      variant='filled'
-      component='a'
-      href='/asda'
+    <SocialButton
       className={classes.github}
+      href='https://github.com/riccardotornesello'
     >
       <IconBrandGithub size='2rem' />
-    </ActionIcon>
+    </SocialButton>
   );
 }
