0f9b6072ddb1d938f96026e107e161987c887f9f
riccardo.tornesello <riccardo.tornesello@gmail.com>
2024-05-09 23:13:21 +0200

    create basic header

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,15 +1,27 @@
 import clsx from "clsx";
 import { clashDisplay } from "@/assets/fonts";
+import avatar from "@/assets/pictures/avatar.png";
+import Circle from "@/components/Circle";
 import styles from "./styles.module.css";
 
 export default function Header() {
   return (
     <header className={clsx(clashDisplay.className, styles.header)}>
-      <h1>
+      <h1 className={styles.title}>
         Riccardo
         <br />
-        Tornesello
+        <span className={styles.surname}>Tornesello</span>
       </h1>
+
+      <div>
+        <Circle
+          radius={120}
+          color="green"
+          borderGap={20}
+          picture={avatar}
+          thickness={4}
+        />
+      </div>
     </header>
   );
 }
