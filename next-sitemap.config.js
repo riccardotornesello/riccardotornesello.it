91592e9c4c718e96368b240b539122e034f5a334
riccardo.tornesello <riccardo.tornesello@gmail.com>
2024-05-09 14:09:14 +0200

    remove old project

--- next-sitemap.config.js
+++
@@ -1,6 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-    siteUrl: process.env.SITE_URL || 'https://riccardotornesello.it',
-    generateRobotsTxt: true,
-    output: 'export',
-}
--- src/data/common.ts
+++
@@ -1,4 +0,0 @@
-export enum Status {
-  FINISHED = 'FINISHED',
-  IN_PROGRESS = 'IN_PROGRESS',
-}
