10287a9cd7de57431be466eecb0024ad90396dd8
riccardo.tornesello <riccardo.tornesello@gmail.com>
2024-05-10 15:20:21 +0200

    improve fonts

--- assets/fonts/index.ts
+++ assets/fonts/index.ts
@@ -1,3 +1,5 @@
 import localFont from "next/font/local";
+import { Outfit } from "next/font/google";
 
 export const clashDisplay = localFont({ src: "./ClashDisplay-Variable.ttf" });
+export const outfit = Outfit({ subsets: ["latin"] });
