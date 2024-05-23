2bdb2fdbd7c5be27d7b65e1d3555fc7dbf00a217
riccardo.tornesello <riccardo.tornesello@gmail.com>
2024-05-23 23:14:46 +0200

    setup static export

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
+
+  images: {
+    unoptimized: true,
+  },
+
   webpack(config) {
     // Grab the existing rule that handles SVG imports
     const fileLoaderRule = config.module.rules.find((rule) =>
