4e8db6a10d769c37805594b065a689f04f940173
riccardo.tornesello <riccardo.tornesello@gmail.com>
2024-05-23 21:36:21 +0200

    new header

--- assets/fonts/index.ts
+++ assets/fonts/index.ts
@@ -1,5 +1,6 @@
 import localFont from "next/font/local";
-import { Outfit } from "next/font/google";
+import { Outfit, Montserrat } from "next/font/google";
 
 export const clashDisplay = localFont({ src: "./ClashDisplay-Variable.ttf" });
 export const outfit = Outfit({ subsets: ["latin"] });
+export const montserrat = Montserrat({ subsets: ["latin"] });
--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,17 +1,18 @@
 import clsx from "clsx";
 import { clashDisplay } from "@/assets/fonts";
-import avatar from "@/assets/pictures/avatar.jpg";
+import avatar from "@/assets/pictures/avatar.png";
 import Circle from "@/components/Circle";
 import styles from "./styles.module.css";
 
 export default function Header() {
+  // TODO: if the h1 goes to the next line, the width of the header should be reduced
+
   return (
     <header className={clsx(clashDisplay.className, styles.header)}>
-      <h1>
-        Riccardo
-        <br />
-        <span className={styles.surname}>Tornesello</span>
-      </h1>
+      <div>
+        <h1>Riccardo Tornesello</h1>
+        <h2>Software Engineer</h2>
+      </div>
 
       <Circle>{avatar}</Circle>
     </header>
