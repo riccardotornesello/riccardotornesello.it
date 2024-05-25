4d83a91a1724c9c61aabf8e94c92323b811a5a7d
riccardo.tornesello <riccardo.tornesello@gmail.com>
2024-05-25 23:55:50 +0200

    add automatic sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  output: "export",
+  siteUrl: "https://riccardotornesello.it",
+  generateRobotsTxt: true,
+  generateIndexSitemap: false,
+};
