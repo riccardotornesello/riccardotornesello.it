187ec728d021d87ef6b2d125261c411f4aae8cdf
Riccardo Tornesello <riccardo.tornesello@gmail.com>
2025-05-01 10:10:38 +0200

    upgrade sentry

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,18 @@
-import type { NextConfig } from "next";
-import { withSentryConfig } from "@sentry/nextjs";
+import { withSentryConfig } from '@sentry/nextjs'
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  output: "export",
-  images: {
-    unoptimized: true,
-  },
-};
+    output: 'export',
+    images: {
+        unoptimized: true,
+    },
+}
 
 export default withSentryConfig(nextConfig, {
-  org: "riccardo-tornesello",
-  project: "riccardotornesello-it",
-  silent: !process.env.CI,
-  widenClientFileUpload: true,
-  hideSourceMaps: true,
-  disableLogger: true,
-  automaticVercelMonitors: true,
-});
+    org: 'riccardo-tornesello',
+    project: 'riccardotornesello-it',
+    silent: !process.env.CI,
+    widenClientFileUpload: true,
+    disableLogger: true,
+    automaticVercelMonitors: true,
+})
--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -8,7 +8,7 @@ import * as Sentry from "@sentry/nextjs";
 Sentry.init({
   dsn: "https://7acd33678c7bb80b4d5e3a7a58f6ee69@o4507319046111232.ingest.de.sentry.io/4507319052599376",
 
-  // Adjust this value in production, or use tracesSampler for greater control
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
   tracesSampleRate: 1,
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
