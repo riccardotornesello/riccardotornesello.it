d780d2cce56a9584fd9ea5a2ab550db2a31fa3a9
Riccardo Tornesello <riccardo.tornesello@gmail.com>
2025-05-24 12:22:44 +0200

    fix build

--- lib/credly.ts
+++ lib/credly.ts
@@ -1,6 +1,22 @@
 import { Certification } from "./types"
 
-export async function getCredlyCertifications() {
+interface CredlyCertification {
+  id: string
+  badge_template: {
+    name: string
+    image_url: string
+  }
+  issuer: {
+    entities: {
+      entity: { name: string }
+    }[]
+  }
+  issued_at_date: string
+}
+
+export async function getCredlyCertifications(): Promise<
+  CredlyCertification[]
+> {
   const res = await fetch(
     "https://www.credly.com/users/riccardotornesello/badges?page=1&page_size=48&sort=rank",
     {
@@ -11,13 +27,14 @@ export async function getCredlyCertifications() {
       method: "GET",
     }
   )
-  return res.json()
+  const data = await res.json()
+  return data.data
 }
 
 export async function parseCredlyCertifications(): Promise<Certification[]> {
   const response = await getCredlyCertifications()
 
-  return response.data.map((c) => ({
+  return response.map((c: CredlyCertification) => ({
     name: c.badge_template.name,
     issuingBody: c.issuer.entities[0].entity.name,
     date: c.issued_at_date.split("-").reverse().join("/"),
